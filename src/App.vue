c6b7c23d6d1061c8542a83d3aef37eb68239e9d8
ageofsys <ageofsys@gmail.com>
2020-10-19 13:34:21 +0900

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ComplianceTable></ComplianceTable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComplianceTable from "@/components/ComplianceTable";
+
+import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ComplianceTable
   }
 }
+
 </script>
 
 <style>
+body {
+  background-color: #fbfbfb;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
